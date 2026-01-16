f47daa2cd57c8acc8bea680bc0397823f8d195c6
xenon0906 <150368103+xenon0906@users.noreply.github.com>
2026-01-16 22:28:09 +0530

    Amazing Commit

--- src/hooks/index.js
+++
@@ -1,4 +0,0 @@
-export { useAutoUpdateGitHub } from './useAutoUpdateGitHub';
-export { useScrollProgress } from './useScrollProgress';
-export { useMousePosition } from './useMousePosition';
-export { useLocalStorage } from './useLocalStorage';
